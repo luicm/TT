6191bc5c98b094f7c5965cebf55d904abab5731f
Luisa Cruz <luisa.cruzmolina@protonmail.com>
2021-02-27 22:01:37 +0100

    Initial set up

---
+++ MusicBrowser/MusicBrowser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicBrowser
+//
+//  Created by Luisa Cruz Molina on 27.02.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicBrowser/MusicBrowser/MusicBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicBrowserApp.swift
+//  MusicBrowser
+//
+//  Created by Luisa Cruz Molina on 27.02.21.
+//
+
+import SwiftUI
+
+@main
+struct MusicBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
