13da90d4ea9bf8f15564b186f5b523daf613bf0f
Luisa Cruz <luisa.cruzmolina@protonmail.com>
2021-03-03 10:03:30 +0100

    App State and Actions

--- MusicBrowser/MusicBrowser/AppState.swift
+++ MusicBrowser/MusicBrowser/AppState.swift
@@ -1,3 +1,77 @@
+//
+//  AppState.swift
+//  MusicBrowser
+//
+//  Created by Luisa Cruz Molina on 27.02.21.
+//
+
+import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+
+/// Controls states and actions that concern logic or modification  of the app globally
+struct AppState: Equatable {
+    var albumns: [Albumn] = []
+    var orderedAlbumns: [Albumn] = []
+    var searchQuery = ""
+}
+
+
+enum AppAction: Equatable {
+    case loadAlbums
+    case loadAlbumsResponse(Result<[Albumn], MusicClient.Failure>)
+    case orderAlbumnsAlphabeticaly([Albumn])
+    case searchQueryChanged(String)
+    case filter(String)
+}
+
+struct AppEnvironment {
+    var client: MusicClient
+    var mainQueue: AnySchedulerOf<DispatchQueue>
+}
+
+
+let appReducer = Reducer<AppState, AppAction, AppEnvironment>.init { (state, action, environment) -> Effect<AppAction, Never> in
+    switch action {
+    
+    case .loadAlbums:
+        return environment.client.requestAlbums()
+            .catchToEffect()
+            .map(AppAction.loadAlbumsResponse)
+        
+    case let .loadAlbumsResponse(.success(albumns)):
+        state.albumns = albumns
+        return Effect(value: .orderAlbumnsAlphabeticaly(albumns))
+        
+    case .orderAlbumnsAlphabeticaly(let albumns):
+        state.orderedAlbumns = albumns.sorted { $0.album < $1.album }
+        return .none
+        
+    case .loadAlbumsResponse(.failure):
+        return .none
+        
+    case let .searchQueryChanged(query):
+        struct SearchAlbumnId: Hashable {}
+        
+        state.searchQuery = query
+        
+        guard !query.isEmpty else {
+            return Effect(value: .orderAlbumnsAlphabeticaly(state.albumns))
+        }
+        
+        return Effect(value: .filter(query))
+            .debounce(id: SearchAlbumnId(), for: 0.3, scheduler: environment.mainQueue)
+        
+    case .filter(let query):
+        let filteredAlbumns = state.albumns.filter { albumn -> Bool in
+           return albumn.album.localizedStandardContains(query)
+        }
+
+        return Effect(value: .orderAlbumnsAlphabeticaly(filteredAlbumns))
+    }
+}
+
 struct MusicClient {
     var requestAlbums: () -> Effect<[Albumn], Failure>
     
