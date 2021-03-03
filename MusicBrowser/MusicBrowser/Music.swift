dbc4ce8796ae7284b01cae7597c2d49fde9da5e5
Luisa Cruz <luisa.cruzmolina@protonmail.com>
2021-03-03 10:04:21 +0100

    Add album placeholder

--- MusicBrowser/MusicBrowser/Music.swift
+++ MusicBrowser/MusicBrowser/Music.swift
@@ -16,3 +16,27 @@ struct Albumn: Codable, Equatable, Identifiable {
     let tracks: [String]
     let year: String
 }
+
+extension Albumn {
+    static func showPlaceholder() -> Albumn {
+        return Albumn(album: "We Are Not Your Kind", artist: "Slipknot",
+                      cover: URL(string: "https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/2b/4d/97/2b4d97a1-d445-c0f9-6f95-f602392ec362/016861741006.jpg/128x128bb.jpeg")!,
+                      id: "1463706038", label: "Roadrunner Records",
+                      tracks: [
+                        "Insert Coin",
+                        "Unsainted",
+                        "Birth of the Cruel",
+                        "Death Because of Death",
+                        "Nero Forte",
+                        "Critical Darling",
+                        "A Liar's Funeral",
+                        "Red Flag",
+                        "What's Next",
+                        "Spiders",
+                        "Orphan",
+                        "My Pain",
+                        "Not Long for This World",
+                        "Solway Firth"
+                      ], year: "2019")
+    }
+}
